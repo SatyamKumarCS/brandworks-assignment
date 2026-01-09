05dfd77973fdc950cdf572b06e834bb9b30babbe
SatyamKumarCS <satyamkumarch15@gmail.com>
2026-01-10 02:24:26 +0530

    fix: cors error

--- server/middleware/setupMiddleware.js
+++ server/middleware/setupMiddleware.js
@@ -2,11 +2,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const setupMiddleware = (app) => {
-  app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  app.use(cors());
   app.use(bodyParser.json());
 };
 
